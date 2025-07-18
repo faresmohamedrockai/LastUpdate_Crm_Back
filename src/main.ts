c98e96f2c49610352a5d90f05435d0e2f733fe5f
faresmohamedrocka <faresmohamed.rockaidev@gmail.com>
2025-07-18 23:30:52 +0300

    production app

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
   app.use(morgan('dev'));
 
  app.enableCors({
-  origin: ['http://localhost:3000','http://localhost:5173'],
+  origin: ['http://localhost:3000','http://localhost:5173','https://propai-crm-front-end.vercel.app'],
   credentials: true,
   methods: 'GET,POST,PUT,DELETE,PATCH,OPTIONS',
   allowedHeaders: 'Content-Type,Authorization,ngrok-skip-browser-warning', // ✅ أضف هذا
