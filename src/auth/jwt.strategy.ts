61563b147f9728ba5e16b78f1e3bd4b07552517d
faresmohamedrocka <faresmohamed.rockaidev@gmail.com>
2025-07-19 17:32:27 +0300

    fareslllll

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -7,14 +7,14 @@ import { ConfigService } from '@nestjs/config';
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(private configService: ConfigService) {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), // 👈 هنا ناخد التوكين من الهيدر
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), 
       ignoreExpiration: false,
       secretOrKey: configService.get<string>('SECERT_JWT_ACCESS') || 'default_secret',
     });
   }
 
   async validate(payload: any) {
-    // ترجع البيانات اللي هتتحط في req.user
+
     return { userId: payload.sub, email: payload.email, role: payload.role };
   }
 }
