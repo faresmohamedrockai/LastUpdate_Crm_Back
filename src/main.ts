ded4114910173d35054f4a7cb70bfdd6a040c52f
rockaidev <rockaidev@gmail.com>
2025-07-24 08:57:23 +0300

    Update CORS configuration to include additional origins

--- src/main.ts
+++ src/main.ts
@@ -16,18 +16,21 @@ async function bootstrap() {
   app.use(cookieParser());
   app.use(morgan('dev'));
 
- app.enableCors({
-  origin: ['http://localhost:3000',
-    'http://localhost:5174',
-    'http://localhost:5173',
-    'https://propai-crm-front-end.vercel.app',
-    'https://propai-crm-front-end-1-coral.vercel.app',
-  'https://propai-crm-front-end-1.vercel.app'],
-  credentials: true,
-  methods: 'GET,POST,PUT,DELETE,PATCH,OPTIONS',
-  allowedHeaders: 'Content-Type,Authorization,ngrok-skip-browser-warning',
-});
-app.useGlobalInterceptors(new BigIntInterceptor());
+  app.enableCors({
+    origin: ['http://localhost:3000',
+      'http://localhost:5174',
+      'http://localhost:5173',
+      'https://propai-crm-front-end.vercel.app',
+      'https://propai-crm-front-end-1-coral.vercel.app',
+      'https://propai-crm-front-end-1.vercel.app',
+      'https://propaicrm.rockaidev.com',
+      'https://redocean.propaicrm.com',
+    ],
+    credentials: true,
+    methods: 'GET,POST,PUT,DELETE,PATCH,OPTIONS',
+    allowedHeaders: 'Content-Type,Authorization,ngrok-skip-browser-warning',
+  });
+  app.useGlobalInterceptors(new BigIntInterceptor());
   app.setGlobalPrefix('api');
 
   app.useGlobalPipes(
