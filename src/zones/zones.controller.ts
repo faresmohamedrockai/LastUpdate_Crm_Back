89c1676745a456051a1a7c678f392d7a38641e4a
rockaidev <rockaidev@gmail.com>
2025-08-20 00:09:13 +0300

    Refactor ZonesController for code consistency
    - Adjusted spacing in constructor and destructured variables for better readability.
    - Ensured consistent formatting across methods in zones.controller.ts.

--- src/zones/zones.controller.ts
+++ src/zones/zones.controller.ts
@@ -10,19 +10,19 @@ import { Role } from '../auth/roles.enum';
 @UseGuards(AuthGuard('jwt'), RolesGuard)
 @Controller('zones')
 export class ZonesController {
-  constructor(private readonly zonesService: ZonesService) {}
+  constructor(private readonly zonesService: ZonesService) { }
 
   @Roles(Role.ADMIN, Role.SALES_ADMIN)
   @Post("create")
   async createZone(@Body() dto: CreateZoneDto, @Req() req) {
-    const {userId,  email, userRole } = req.user;
+    const { userId, email, userRole } = req.user;
     return this.zonesService.createZone(dto, userId, email, userRole);
   }
 
   @Roles(Role.ADMIN, Role.SALES_ADMIN, Role.TEAM_LEADER, Role.SALES_REP)
   @Get()
   async getAllZones(@Req() req) {
-    const {  userId, email, role } = req.user;
+    const { userId, email, role } = req.user;
     return this.zonesService.getAllZones(userId, email, role);
   }
 
@@ -35,14 +35,14 @@ export class ZonesController {
     @Body() dto: UpdateZoneDto,
     @Req() req
   ) {
-    const { userId, email,role } = req.user;
+    const { userId, email, role } = req.user;
     return this.zonesService.updateZone(id, dto, userId, email, role);
   }
 
   @Roles(Role.ADMIN)
   @Delete(':id')
   async deleteZone(@Param('id') id: string, @Req() req) {
-    const {  userId, email, role } = req.user;
+    const { userId, email, role } = req.user;
     return this.zonesService.deleteZone(id, userId, email, role);
   }
 } 
